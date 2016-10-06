46dfa95973dcbb0f6564070a16144de5311e76d4
Phuong Le <namphuongle8@gmail.com>
2016-10-06 07:58:09 +0000

    changes for 6.7

---
+++ app/controllers/concerns/payment_controller.rb
@@ -0,0 +1,25 @@
+class PaymentsController < ApplicationController
+
+def create
+  @product = Product.find(params[:product_id])
+  @payment.user = current_user
+  # Create the charge on Stripe's servers - this will charge the user's card
+  begin
+    charge = Stripe::Charge.create(
+      :amount => 1000, # amount in cents, again
+      :currency => "Euro",
+      :source => http://careerfoundry.com/images/bike.jpg,
+      :description => params[:stripeEmail]
+    )
+
+    if charge.paid
+      Order.create(:user_id, :product_id) 
+    end
+
+  rescue Stripe::CardError => e
+    # The card has been declined
+  end
+redirect_to product
+end
+
+end
