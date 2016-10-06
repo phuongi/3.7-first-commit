d70fcdc991feccbde91a2cc38e35c6f7b83cb865
Phuong Le <namphuongle8@gmail.com>
2016-10-06 16:31:26 +0000

    error 6.7 fix

--- app/controllers/payment_controller.rb
+++ app/controllers/payment_controller.rb
@@ -3,8 +3,7 @@ class PaymentsController < ApplicationController
 def create
   @product = Product.find(params[:product_id])
   @payment.user = current_user
-  redirect_to product
-  # Create the charge on Stripe's servers - this will charge the user's card
+    # Create the charge on Stripe's servers - this will charge the user's card
   begin
     charge = Stripe::Charge.create(
       :amount => 1000, # amount in cents, again
