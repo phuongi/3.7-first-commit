2f7ff12fe7b49ce4e355082fc5c82923cfb2d096
Phuong Le <namphuongle8@gmail.com>
2016-10-10 14:16:34 +0000

    for 5.3

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,16 @@
 class StaticPagesController < ApplicationController
-def landing_page
- 	@featured_product = Product.first
-	@products = Product.limit(3)
-end
+	def landing_page
+ 		@featured_product = Product.first
+		@products = Product.limit(3)
+	end
+
+	def thank_you
+    	 @name = params[:name]
+ 		 @email = params[:email]
+ 		 @message = params[:message]
+  		ActionMailer::Base.mail(:from => @email,
+   	     :to => 'namphuongle8@gmail.com',
+         :subject => "A new contact form message from Nil",
+         :body => @message).deliver_now
+	end
 end
