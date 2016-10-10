30f2ea9bbb3fc354e6426aae912f628305363199
Phuong Le <namphuongle8@gmail.com>
2016-10-10 15:38:17 +0000

    6.2 error

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,17 +1,17 @@
 FactoryGirl.define do
-sequence(:email) { |n| "user#{n}@example.com" }
+  sequence(:email) { |n| "user#{n}@example.com" }
   factory :user do
     email 
-    password ...
-    first_name 
+    password "123456"
+    first_name "Test"
     last_name "Example"
     admin false
   end
 
   factory :admin, class: User do
     email 
-    password ...
-    admin 
+    password "123456"
+    admin true
     first_name "Admin"
     last_name "User"
   end
--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -1,18 +1,11 @@
 require 'rails_helper'
 describe Product do
+    before do
+     @product = Product.create!(name: "Mountain Bikes")
+     @user = User.create!(email: "namphuong@gmail.com", password: "namphuong")
+     @product.comments.create!(rating: 1, user: @user, body: "Awful bike!")
+    end
 
-
-before do
-  @product = Product.create!(name: "Mountain Bikes")
-end
-
-before do
-  @user = User.create!(email: "felix@gmail.com", password: "felix")
-end
-
-it "returns the average rating of all comments" do
-end
-
-@product.comments.create!(rating: 1, user: @user, body: "Awful bike!")
-
+    it "returns the average rating of all comments" do
+    end
 end
